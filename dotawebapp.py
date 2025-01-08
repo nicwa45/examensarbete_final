108ca593fd1a2930c236c111ba9060897c1f4684
nicwa45 <147014107+nicwa45@users.noreply.github.com>
2025-01-08 12:34:39 +0100

    Update dotawebapp.py
    TESTING api

--- dotawebapp.py
+++ dotawebapp.py
@@ -3,12 +3,13 @@ import requests
 import os
 
 app = Flask(__name__)
-API_KEY = os.getenv("API_KEY")
+#API_KEY = os.getenv("API_KEY")
 #API Base URLs and Authentication
 API_BASE_URL = "https://api.opendota.com/api"
 STRATZ_GRAPHQL_URL = "https://api.stratz.com/graphql"
 #STRATZ_AUTH_TOKEN = os.getenv("STRATZ_AUTH_TOKEN")
-STRATZ_AUTH_TOKEN = API_KEY
+#STRATZ_AUTH_TOKEN = API_KEY
+STRATZ_AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdWJqZWN0IjoiYmIwYjcwZGItMzNlYi00OGUzLThhYjUtNWNmZmEzYjhiMTc1IiwiU3RlYW1JZCI6IjI2ODY0ODM0IiwibmJmIjoxNzMzMTU1OTc5LCJleHAiOjE3NjQ2OTE5NzksImlhdCI6MTczMzE1NTk3OSwiaXNzIjoiaHR0cHM6Ly9hcGkuc3RyYXR6LmNvbSJ9.J4n-S_sHCAAwgz7MR_rNUIr61dfgLqZqZV_vA_R9qaI"
 
 @app.route('/')
 def index():
